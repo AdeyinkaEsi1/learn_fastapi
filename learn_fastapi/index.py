a1857f0884cd381399492b44d6480e8cdb0ae675
AdeyinkaEsi1 <yhamid2828@gmail.com>
2024-02-05 23:17:41 +0100

    passing an input

--- learn_fastapi/index.py
+++ learn_fastapi/index.py
@@ -8,7 +8,6 @@ app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
-@app.get('/index/', response_class=HTMLResponse)
-def index(request: Request):
-    context = {'request': request}
-    return templates.TemplateResponse('index.html', context)
+@app.get('/index/{pageid}')
+async def index(pageid):
+    return {'pageid': pageid}
