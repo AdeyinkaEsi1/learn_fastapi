aabdfbe14db15b3b023e371590c9e6541ecd0b89
AdeyinkaEsi1 <yhamid2828@gmail.com>
2024-02-05 21:53:22 +0100

    mod

--- learn_fastapi/main.py
+++ learn_fastapi/main.py
@@ -1,35 +1,8 @@
-# from fastapi import FastAPI
-# from typing import Union
-# from pydantic import BaseModel
-
-# app = FastAPI()
-
-# class Item(BaseModel):
-#     name: str
-#     price: float
-#     is_offer: Union[bool, None] = None
-
-# @app.get('/')
-# def read_root():
-#     return {'Hello': 'World'}
-
-
-# @app.get('/items/{item_id}')
-# def read_item(item_id: int, q: Union[str, int] = None):
-#     return {'item_id': item_id, 'q': q}
-
-
-# @app.put('/items/{item_id}')
-# def update_item(item_id: int, item: Item):
-#     return {'item_name': item.name, 'item_id': item_id}
-
-
-
 from fastapi import FastAPI
-import add_router as ar
-
+import add_router
 app = FastAPI()
-app.include_router(ar.router)
+app.include_router(add_router.router)
+
 
 
 @app.get('/')
@@ -39,16 +12,14 @@ def home():
     }
 
 
-@app.get('/add/numbers')
-def adding_numbers():
-    return {
-        "message": 'Addition of numbers page'
-    }
+# @app.get('/numbers')
+# def adding_numbers():
+#     return f'message(s): Addition of numbers page'
 
 
-@app.get('/add/strings')
-def add_strings():
-    return {
-        'message': 'addition of strings page'
-    }
+# @app.get('/strings')
+# def add_strings():
+#     return {
+#         'message': 'addition of strings page'
+    # }
 
